4eccd07a00a425b8a5049e24fa2fac1d78bce16d
Julia <julia.hlukhotorenko@xa.epicgames.com>
2024-03-07 08:48:31 +0100

    task2

---
+++ task2.cjs
@@ -0,0 +1,31 @@
+const { exec } = require('child_process');
+const os = require('os');
+const fs = require('fs');
+let dataToSave = '';
+const command = os.type() === 'Windows_NT' 
+    ? 'powershell "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + \' \' + $_.CPU + \' \' + $_.WorkingSet }"'
+    : 'ps -A -o %cpu,%mem,comm | sort -nr | head -n 1';
+
+setInterval(() => {
+    console.clear();
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+
+        console.log(stdout);
+        dataToSave = `${dataToSave}${(new Date).toUTCString()}:${stdout}`;
+    });
+}, 100);
+
+setInterval(() => {
+    fs.writeFile('activityMonitor.log', dataToSave, (err) => {
+        if (err) throw err;
+    });
+    dataToSave = '';
+}, 60000);
